53f3306511ca2ba73d0c8a86c2fcb071419e27a3
Karl <52241072+kungfury100@users.noreply.github.com>
2025-08-25 21:15:54 +0300

    Welp

--- app/error.tsx
+++ app/error.tsx
@@ -16,18 +16,16 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="min-h-[87vh] px-2 sm:py-28 py-36 flex flex-col gap-4 items-center">
+    <div className="min-h-[87vh] px-2 sm:py-28 py-36 flex flex-col gap-8 items-center">
       <div className="text-center flex flex-col items-center justify-center w-fit gap-2">
-        <h2 className="text-7xl font-bold pr-1">Oops!</h2>
-        <p className="text-muted-foreground text-md font-medium">
-          Something went wrong {":`("}
-        </p>
-        <p>
+        <h2 className="text-primary text-7xl font-bold pr-1">Oops!</h2>
+        <p className="text-primary text-md font-medium">
           We&apos;re sorry, but an error occurred while processing your request.
         </p>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         <Button
+          className={buttonVariants({ size: "xl" })}
           onClick={
             // Attempt to recover by trying to re-render the segment
             () => reset()
@@ -35,8 +33,8 @@ export default function Error({
         >
           Reload page
         </Button>
-        <Link href="/" className={buttonVariants({})}>
-          Back to homepage
+        <Link href="/" className={buttonVariants({ size: "xl" })}>
+          Back to Moon
         </Link>
       </div>
     </div>
