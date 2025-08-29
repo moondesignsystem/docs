ab303f839c40fd3e87e30bdb1d3ef486f1d9f5ce
Karl <52241072+kungfury100@users.noreply.github.com>
2025-08-29 14:02:24 +0300

    Updated basepath conditions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
 
-  basePath: "/moon-docs",
+  basePath: isProd ? "/moon-docs" : "",
 
   images: {
     unoptimized: true,
